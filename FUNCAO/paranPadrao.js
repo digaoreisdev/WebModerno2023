463595219c16d2426bb17d128fce6a8fbe3b5083
Digao Reis <digaoreis.dev@gmail.com>
2023-10-13 17:45:46 -0300

    Função 2

---
+++ FUNCAO/paranPadrao.js
@@ -0,0 +1,16 @@
+// Parâmetro Padrão
+// Estratégia 1 para gerar valor padrão
+function soma1(a, b, c) {
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    
+    return a + b + c
+}
+
+console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
+
+// Estratégia 2, 3 e 4 para gerar valor padrão
+function soma2(a, b, c) {
+    a = a !==
+}
