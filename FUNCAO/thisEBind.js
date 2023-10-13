b01aac0a9164d83dacfcc58fd37bf7ae4563e253
Digao Reis <digaoreis.dev@gmail.com>
2023-10-13 20:09:55 -0300

    This e Bind

---
+++ FUNCAO/thisEBind.js
@@ -0,0 +1,14 @@
+// This e Bind
+const pessoa = {
+    saudacao: 'Bom dia!',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // conflito entre paradigmas: funcional e Orientação a Objetos
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
