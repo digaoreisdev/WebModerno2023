25c35b8148957a752d558d40a7962659d2d4c987
Digao Reis <digaoreis.dev@gmail.com>
2023-10-24 21:15:16 -0300

    Node.js

---
+++ Node-JS/moduloA.js
@@ -0,0 +1,5 @@
+/* NODE - Sistemas de Móduloas - Módulo A */
+// Formas de Exportar de dentro do NODE para outros arquivos
+this.ola = 'Fala Pessoal!'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.atelogo = 'Até o próximo exemplo.'
