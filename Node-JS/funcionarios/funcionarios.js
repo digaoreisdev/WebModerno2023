43198d935882ade5a9eb3532b6a38977f98e7254
Digão Reis <digaoreis.dev@gmail.com>
2023-11-08 12:50:50 -0300

    Continuando

--- Node-JS/funcionarios/funcionarios.js
+++ Node-JS/funcionarios/funcionarios.js
@@ -9,7 +9,7 @@ const menorSalario = (func, funcAtual) => {
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    // console.log(funcionarios)
+    //console.log(funcionarios)
     
     //Mulher Chinesa com o menor salário?
     const func = funcionarios
